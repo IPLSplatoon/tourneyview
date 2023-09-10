1ded91b03056851cca975166dfa118120df70f7b
inkfarer <inkfarer@inkfarer.com>
2023-09-10 13:06:38 +0300

    importer: More generic return type for MatchImporter.getBracketQuery

--- packages/importer/lib/types/MatchImporter.ts
+++ packages/importer/lib/types/MatchImporter.ts
@@ -1,8 +1,8 @@
 import { Bracket } from '@tourneyview/common';
-import { BracketQuerySelectParameter } from './BracketQuery';
+import { BracketQueryParameter } from './BracketQuery';
 
 export interface MatchImporter<ImportOpts> {
-    getBracketQuery(tournamentId: string): Promise<BracketQuerySelectParameter[]> | BracketQuerySelectParameter[]
+    getBracketQuery(tournamentId: string): Promise<BracketQueryParameter[]> | BracketQueryParameter[]
 
     getMatches(opts: ImportOpts): Promise<Bracket> | Bracket
 }
